0b832c1aef0a0bd4990d2f1fa0bf39ddd2d61498
Ludrachel <ludmylaraquel0501@gmail.com>
2023-04-27 12:40:43 -0300

    first commit

---
+++ compraparcelada.py
@@ -0,0 +1,8 @@
+valor = int(input())
+for i in range(1, 25):
+    x=valor/i
+    print(str(i)+ "x " + "de " +"R$ "+ f'{x:.2f}')
+
+
+
+
---
+++ ppp.py
@@ -0,0 +1,8 @@
+import requests
+
+url="https://run.codes/home"
+
+
+page=requests.get(url)
+
+print(page)
